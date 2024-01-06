8e499ed0855e7676d5f6a319e94f57cca2f2a223
pallavbadhiyal <pallavbadhiyal@gmail.com>
2024-01-06 15:44:22 +0530

    ip_velidation_class_check

--- 3jan.py
+++
@@ -1,8 +0,0 @@
-ip = input("Please provide your ip address: ")
-ip_address = ip.split(".")
-size = len(ip_address)
-if size==4 and all (0 < int(ip) <= 255 for ip in ip_address):
-    print("ip_address is a Valid IP")
-else    :    
-    print("ip_address is not a Valid IP")
-
