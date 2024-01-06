7a822156cfcd90457ff825774e9cfe939d5a40ea
pallavbadhiyal <pallavbadhiyal@gmail.com>
2024-01-06 15:17:09 +0530

    with less lines

--- 3jan.py
+++ 3jan.py
@@ -5,13 +5,19 @@ size = len(ip_address)
 if size != 4:
     print("ip_address is not a Valid IP")
 else:
-    if all (0 < int(ip) <= 255 for ip in ip_address):
-    # if int(ip_address[1]) <= 255 and int(ip_address[1]) >= 0:
-    #     if int(ip_address[2]) <= 255 and int(ip_address[2]) >= 0:
-    #         if int(ip_address[3]) <= 255 and int(ip_address[3]) >= 0:
-        print("ip_address is a Valid IP")
-    else    :    
-        print("ip_address is not a Valid IP")
+ if all (0 < int(ip) <= 255 for ip in ip_address):
+    print("ip_address is a Valid IP")
+ else    :    
+    print("ip_address is not a Valid IP")
+
+
+
+
+
+
+
+
+
 
  
 #  for ip in ip_address:
@@ -20,7 +26,6 @@ else:
 #             continue         
 #         else:    
 #             False
-    
 # if ip:
 #     print("ip_address is a Valid IP")
 # else:    
